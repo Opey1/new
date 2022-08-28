194d320d261437fac1ca87bcbb4c8f7a811631ba
victor <falolao26@gmail.com>
2022-08-28 15:19:02 +0100

    commit

---
+++ another_new/hello.py
@@ -0,0 +1,8 @@
+#this is my python hello world.
+print("hello world")
+c=int(input("Please enter a number "))
+print(c*6)
+#you can actually create function 
+d=int(input("please enter another number ")
+f=c*c+d*d
+print(f)
